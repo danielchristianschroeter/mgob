24333b611c60b97c9335ceb75434d0d033230d2f
Daniel Christian Schroeter <2836188+danielchristianschroeter@users.noreply.github.com>
2024-11-06 20:48:56 +0100

    cleanup required changed for testing for PR to mgob repo

--- build.sh
+++ build.sh
@@ -102,7 +102,9 @@ install_cli_tools() {
     echo "Installing Azure CLI..."
     pip install --no-cache-dir "azure-cli==${AZURE_CLI_VERSION}"
     ln -s /opt/venv/bin/az /usr/bin/az
+    /usr/bin/az config set auto-upgrade.enable=no
     /usr/bin/az config set auto-upgrade.prompt=no
+    /usr/bin/az config set core.survey_message=false
   fi
 
   # AWS CLI
